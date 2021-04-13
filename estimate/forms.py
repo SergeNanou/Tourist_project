1bc115f12f8119501fb4362b58e323ef2a59d2c1
serge.nanou <serge.nanou@eisti.eu>
2021-04-13 20:51:09 +0200

    com_13_04

--- estimate/forms.py
+++ estimate/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class EstimateForm(forms.Form):
-    Localité = forms.CharField(max_length=100)
-    Nombre_de_personnes  = forms.IntegerField()
-    Type_d_hotel  = forms.IntegerField()
-    Forme_de_tourisme = forms.CharField(max_length=100)
-    Emails = forms.EmailField()
-    Nombre_de_lieux_a_visiter  = forms.IntegerField()
-    Types_de_transport = forms.CharField(max_length=100)
-    Commentaires = forms.CharField(widget=forms.Textarea)
+    localite = forms.CharField(max_length=100)
+    nombre_de_personnes  = forms.IntegerField()
+    nbre_etoile_hotel  = forms.IntegerField()
+    forme_de_tourisme = forms.CharField(max_length=100)
+    emails = forms.EmailField()
+    nombre_de_lieux_a_visiter  = forms.IntegerField()
+    types_de_transport = forms.CharField(max_length=200)
+    commentaires = forms.CharField(widget=forms.Textarea)
--- estimate/models.py
+++ estimate/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Estimate(models.Model):
+    user = models.CharField(max_length=30)
+    localite = models.CharField(max_length=100)
+    nombre_de_personnes  = models.IntegerField()
+    nbre_etoile_hotel  = models.IntegerField()
+    forme_de_tourisme = models.CharField(max_length=100)
+    emails = models.EmailField()
+    nombre_de_lieux_a_visiter  = models.IntegerField()
+    types_de_transport = models.CharField(max_length=200)
+    commentaires = models.TextField(max_length=500)
+    
+    class Meta:
+        verbose_name = "Message"
+
+    def __str__(self):
+        return '{} {} {} {} {} {}'.format(self.user,
+                                          self.localite,
+                                          self.nombre_de_personnes,
+                                          self.nbre_etoile_hotel,
+                                          self.forme_de_tourisme,
+                                          self.emails,
+                                          self.nombre_de_lieux_a_visiter,
+                                          self.types_de_transport,
+                                          self.commentaires)
